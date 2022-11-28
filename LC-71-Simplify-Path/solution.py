f8c213b9c64bc09ce524e2c3e9b4d2661e3ad90a
Ian Akotey <ianakotey@gmail.com>
2022-11-28 08:52:19 +0000

    Added LC 71

---
+++ LC-71-Simplify-Path/solution.py
@@ -0,0 +1,40 @@
+
+from pathlib import Path
+
+
+class Solution:
+    def simplifyPath(self, path: Path | str) -> str:
+
+
+        valid_chunks = []
+        
+        for chunk in str(path).split("/"):
+
+            match chunk:
+
+                case '' | '.': # multiple / (eg. ////... ) or cwd
+                    pass
+                case '..':
+                    try:
+                        valid_chunks.pop() # go
+                    except IndexError: 
+                        pass # /../../.. is valid
+
+                case _:
+                    valid_chunks.append(chunk)
+
+        return '/'+'/'.join(valid_chunks)
+
+
+def test():
+    
+    test_runner = Solution()
+
+    test_path_str = '///tmp/../tmp/./././test.txt'
+    test_path = Path(test_path_str)
+
+    assert test_runner.simplifyPath(test_path) \
+        == test_runner.simplifyPath(test_path_str) \
+            == '/tmp/test.txt'
+
+test()
