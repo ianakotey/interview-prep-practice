0f64ea2aba3a5513a2cd6aed299a555635a87057
Ian Akotey <ianakotey@gmail.com>
2022-11-29 05:05:30 +0000

    Added LC-50

---
+++ LC-50-Pow(x,n)/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        match n:
+            case y if y < 0:
+                return 1/self.myPow(x, -y)
+            case 0:
+                return 1
+            case 1:
+                return x
+            case 2:
+                return x * x
+            case _:
+                res = self.myPow(x, n//2)
+                return res * res if n % 2 == 0 else res * res * x
+
+        raise RuntimeError("Unreachable!")
+
+def test():
+
+    test_runner = Solution()
+
+    for i in range(-5, 5):
+        print(test_runner.myPow(2, i))
+
+
+test()
